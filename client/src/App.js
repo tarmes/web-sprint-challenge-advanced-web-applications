f85bb6016663e6eb39b8c2896f798687026c1117
tarmes <traarmes@gmail.com>
2020-10-16 11:10:51 -0400

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/bubble-page">Bubbles</Link>
+          </li>
+        </ul>
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
